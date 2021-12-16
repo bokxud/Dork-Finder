8b2f23507bd51f49baca2a773f386eabf0e31e5e
bokxud <45327283+bokxud@users.noreply.github.com>
2021-12-17 00:31:58 +0600

    Add files via upload

---
+++ dorks-Finder.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# A notice to all nerds and n00bs...
+# If you will copy the developer's work it will not make you a hacker..!
+# Respect all developers, we doing this because it's fun...
+# So please, don't copy this code.
+
+
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
+os.system('clear')
+print """
+\033[1;91m
+
+
+
+                ___    ___    _   _  ___      __      _   
+/'\_/`\         (  _`\ (  _`\ ( ) ( )|  _`\  /'__`\  /'_`\ 
+|     | _ __    | (_) )| | ) || |/'/'| (_) )(_)  ) )( (_) )
+| (_) |( '__)   |  _ <'| | | )| , <  | ,  /    /' /  > _ <'
+| | | || |    _ | (_) )| |_) || |\`\ | |\ \  /' /( )( (_) )
+(_) (_)(_)   (_)(____/'(____/'(_) (_)(_) (_)(_____/'`\___/'
+                                                           
+                                                           
+               
+                                                                                  
+                                                                              
+
+                          """
+print("create \033[1;92mMr. BDKR28")
+
+time.sleep(3)
+print ""
+print ""
+
+CorrectUsername = "BDKR28"
+CorrectPassword = "dork-finder"
+
+loop = 'true'
+while (loop == 'true'):
+    username = raw_input("Tool Username : ")
+    if (username == CorrectUsername):
+    	password = raw_input("Tool Password : ")
+        if (password == CorrectPassword):
+            print "Logged in successfully as " + username #Dev:BDKR28_SANA_FAROOQ
+            os.system('xdg-open https://www.facebook.com/ctfsolution')
+            
+	    time.sleep(5)
+            loop = 'false'
+        else:
+            print "\033[1;93mWrong Password"
+            os.system('xdg-open https://www.facebook.com/ctfsolution')
+    else:
+        print "\033[1;94mWrong Username"
+        os.system('xdg-open https://www.facebook.com/ctfsolution')
+        
+        
+os.system('clear')
+      
+        
+os.system ('python3 ...')
